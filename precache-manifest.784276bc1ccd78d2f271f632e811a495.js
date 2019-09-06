1a79005b0fa8f0855582efb63c9fd4ec80679199
vishalsahu30081994 <50576178+vishalsahu30081994@users.noreply.github.com>
2019-09-06 23:39:50 +0530

    My First-React-app

---
+++ precache-manifest.784276bc1ccd78d2f271f632e811a495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91224e94f43d13f3b741a6463589d1ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d25f3cb2288d48bde4d",
+    "url": "/static/css/main.27becd0b.chunk.css"
+  },
+  {
+    "revision": "81292e02b5f87db12621",
+    "url": "/static/js/2.6b5800a1.chunk.js"
+  },
+  {
+    "revision": "6d25f3cb2288d48bde4d",
+    "url": "/static/js/main.2602a131.chunk.js"
+  },
+  {
+    "revision": "bc30cf0f329c697925ab",
+    "url": "/static/js/runtime~main.6bad53bd.js"
+  },
+  {
+    "revision": "04fe7da2517914cad41913b8ab48e4bb",
+    "url": "/static/media/back7.04fe7da2.jpg"
+  }
+]);
